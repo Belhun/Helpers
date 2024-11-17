1a2763e10a48aa3e5d9c8bc952b0cab78ac25d3a
Belhun <akcool151@gmail.com>
2024-11-17 00:45:20 -0800

    Add Construction_helping class with TODOs for research and and adding Contriction helpers

---
+++ Source/Helpers/Construction helping.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    internal class Construction_helping
+    {
+        //TODO : Research how construction normally works and find where the best location to add helpers would be
+        //TODO : Add construction helpers
+
+    }
+}
